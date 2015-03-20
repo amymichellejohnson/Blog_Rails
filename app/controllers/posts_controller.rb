3d4058329ca82ea6f4af9e68347c8ba8e28eb35c
Amy Michelle Johnson <amyjohnsonmichelle@gmail.com>
2015-03-20 10:58:34 -0700

    Add CRUD/L functionality for Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,35 @@ class PostsController <ApplicationController
     end
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "Post successfully edited!"
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:alert] = "Post deleted"
+    redirect_to posts_path
+  end
+
+
+
+
+
   private
     def post_params
       params.require(:post).permit(:title, :date, :blog)
